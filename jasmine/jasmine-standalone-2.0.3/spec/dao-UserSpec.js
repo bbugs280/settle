76d944c355da1c090243553d0ded10c84a1f729e
bbugs280 <yeung.vincent@gmail.com>
2014-09-24 23:43:19 +0800

    update from iMac

--- jasmine/jasmine-standalone-2.0.3/spec/dao-UserSpec.js
+++ jasmine/jasmine-standalone-2.0.3/spec/dao-UserSpec.js
@@ -1,5 +1,6 @@
 describe("DAO User", function() {
     var user;
+    var checkresult;
     describe("Login: ", function(){
 
         beforeEach(function(done){
@@ -14,10 +15,52 @@ describe("DAO User", function() {
         it("should login successful", function() {
             expect(user).toBeDefined();
             expect(user.username).toMatch('yeung.vincent@gmail.com');
-//            console.log(" user "+ user.username);
-//            done();
+
         });
     })
 
+    describe("getUSER by email: ", function(){
+
+        beforeEach(function(done){
+            user = new User();
+            user.getUser('yeung.vincent@gmail.com', function (result) {
+                checkresult = result;
+                done();
+            })
+
+        });
+
+        it("should get User successful", function() {
+            expect(checkresult).toBeDefined();
+//            expect(user.username).toMatch('yeung.vincent@gmail.com');
+
+        });
+    })
+//    describe("Save: ", function(){
+//
+//        beforeEach(function(done){
+//            user = new User();
+//            user.username="abc";
+//            user.email="you@you.com";
+//            user.password="123";
+//            user.save(function (result) {
+//                checkresult = result;
+//                done();
+//            })
+//
+//        });
+//
+//        it("should save successful", function() {
+//            console.log(" user "+ user.toString());
+//            expect(user).toEqual(checkresult);
+//
+//
+//        });
+//
+//        afterEach(function(done){
+//
+//            done();
+//        })
+//    })
 
 });
