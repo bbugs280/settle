79377dd1a991f633c3193ac51a61521b72c37acc
bbugs280 <yeung.vincent@gmail.com>
2014-11-25 00:16:24 +0800

    image resize improvement

---
+++ platforms/android/ant-build/CordovaApp-release-unsigned.apk.d
@@ -0,0 +1,8 @@
+/Users/vincent/Documents/SoftDev/settle/platforms/android/ant-build/CordovaApp-release-unsigned.apk : \
+/Users/vincent/Documents/SoftDev/settle/platforms/android/ant-build/CordovaApp.ap_ \
+/Users/vincent/Documents/SoftDev/settle/platforms/android/ant-build/classes.dex \
+/Users/vincent/Documents/SoftDev/settle/platforms/android/CordovaLib/ant-build/classes.jar \
+/Users/vincent/Documents/SoftDev/settle/platforms/android/libs/Parse-1.3.8.jar \
+/Users/vincent/Documents/SoftDev/settle/platforms/android/libs/com.google.zxing.client.android.captureactivity.jar \
+/Users/vincent/Documents/SoftDev/settle/platforms/android/libs/gcm.jar \
+/Users/vincent/Documents/SoftDev/settle/platforms/android/libs/android-support-v13.jar \
